6edae5f06b7d3bf157f4c1902e57206c5a625fa8
FadyShakerShawky <fadyshakershawky@gmail.com>
2021-05-05 01:46:00 +0200

    finished students profile and timezone

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Auth;
 
 class StudentsController extends Controller
 {
@@ -11,5 +12,16 @@ class StudentsController extends Controller
     {
         $data = Student::find($id);
         return view('student-profile', ["data"=>$data]);
+    }
+    public function update(){
+        if (isset($_REQUEST['government']) && isset($_REQUEST['birthday']) ){
+          
+            $student = Auth::user()->students;
+            $student->government = $_REQUEST['government'];
+            $student->birthday = $_REQUEST['birthday'];
+            $student->save();
+            return redirect()->route("student-profile" , ["id" => Auth::user()->students->id ]);
+        }
+       
     }
 }
